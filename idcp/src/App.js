b2fb27898418b142f61b5e45f8c50ba3a055af90
Erickson Ruck <98894958+Erickson98@users.noreply.github.com>
2022-09-17 04:36:19 -0400

    set up

---
+++ idcp/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Vector from "./assests/Vector.jpg";
+import "./s.css";
+function App() {
+  return (
+    <div>
+      <img src={Vector} class="max-w-full h-screen w-full bg-black" />
+    </div>
+  );
+}
+
+export default App;
